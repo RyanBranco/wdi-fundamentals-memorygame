f83698cfe5c3265052b7d8d16a2087f2334a0563
Ryan Branco <ryanbr23@gmail.com>
2019-12-08 17:52:49 -0800

    added funtions

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,15 +1,16 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("sorry try again")
-    }
-}
+    };
+};
+
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+    checkForMatch();
+};
